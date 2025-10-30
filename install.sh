5df650d9da4c445a1dd2d2ee23413480499025ce
grigiot <175511406+grigiot@users.noreply.github.com>
2025-10-30 23:38:08 +0100

    fix: proper source of ros2_setup in rc file

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp ./ros2_setup "${HOME}/.ros2_setup.${SN}"
 if grep -q "source .ros2_setup.${SN}" "${HOME}/.${SN}rc"; then
   echo ".ros2_setup.${SN} already present. No need to write 'source .ros2_setup.${SN}' in your $HOME/.${SN}rc"
 else
-  echo "'source .ros2_setup.${SN}' >> ${HOME}/.${SN}rc"
-  echo "source .ros2_setup.${SN}" >> "${HOME}/.${SN}rc"
+  echo "'source ${HOME}/.ros2_setup.${SN}' >> ${HOME}/.${SN}rc"
+  echo "source ${HOME}/.ros2_setup.${SN}" >> "${HOME}/.${SN}rc"
 fi
 
